82796eaf8c347f882b81beffe315388962037e9a
sinbox0701 <psh090953@gmail.com>
2021-04-22 10:53:33 +0900

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
+
 function App() {
+  const isLoggedIn = true;
+  //한번에 한 URL만 Render --> swtich역할
   return (
     <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {isLoggedIn ? <Home/> : <Login/>}
+          </Route>
+          <Route>
+            <NotFound/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
