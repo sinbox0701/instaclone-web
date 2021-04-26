1e4bb308679dbf97242bfd5268d6f412b315298d
sinbox0701 <psh090953@gmail.com>
2021-04-26 11:18:37 +0900

    Avatar

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ import styled from "styled-components";
 import { isLoggedInVar } from "../apollo";
 import useUser from "../hooks/useUser";
 import routes from "../routes";
+import Avatar from "./Avatar";
 
 const SHeader = styled.header`
     width: 100%;
@@ -48,9 +49,14 @@ const Button = styled.span`
   font-weight: 600;
 `;
 
+const IconsContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 function Header(){
     const isLoggedIn = useReactiveVar(isLoggedInVar);
-    const loggedInUser = useUser();
+    const {data} = useUser();
     return (
         <SHeader>
             <Wrapper>
@@ -59,17 +65,17 @@ function Header(){
                 </Column>
                 <Column>
                     {isLoggedIn ? (
-                        <>
-                        <Icon>
-                            <FontAwesomeIcon icon={faHome} size="lg" />
-                        </Icon>
-                        <Icon>
-                            <FontAwesomeIcon icon={faCompass} size="lg" />
-                        </Icon>
-                        <Icon>
-                            <FontAwesomeIcon icon={faUser} size="lg" />
-                        </Icon>
-                        </>
+                        <IconsContainer>
+                            <Icon>
+                                <FontAwesomeIcon icon={faHome} size="lg" />
+                            </Icon>
+                            <Icon>
+                                <FontAwesomeIcon icon={faCompass} size="lg" />
+                            </Icon>
+                            <Icon>
+                                <Avatar url={data?.me?.avatar} />
+                            </Icon>
+                        </IconsContainer>
                     ) : (
                         <Link href={routes.home}>
                             <Button>Login</Button>
