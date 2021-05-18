263b79d3034eee67bdabfce42094c6261099263f
sinbox0701 <psh090953@gmail.com>
2021-05-18 13:44:29 +0900

    apollo

--- src/apollo.js
+++ src/apollo.js
@@ -25,7 +25,7 @@ export const enableDarkMode = () => {
 };
 
 const httpLink = createHttpLink({
-    uri:"http://localhost:4000/graphql",
+    uri: process.env.NODE_ENV === "production" ? "https://instaclone-back.herokuapp.com/graphql" : "http://localhost:4000/graphql",
 });
 
 const authLink = setContext((_,{headers})=>{
